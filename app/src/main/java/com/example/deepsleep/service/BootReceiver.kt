fe78028f53c50b7d65be5088f66118904801de21
yuzhoulandawang <814745511@qq.com>
2026-02-25 07:02:18 +0800

    更新 BootReceiver.kt

--- app/src/main/java/com/example/deepsleep/service/BootReceiver.kt
+++ app/src/main/java/com/example/deepsleep/service/BootReceiver.kt
@@ -7,7 +7,6 @@ import android.content.Intent
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            // 开机自启（默认启用）
             context.startService(Intent(context, DeepSleepService::class.java).apply {
                 action = DeepSleepService.ACTION_START
             })
